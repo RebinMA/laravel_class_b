d4caa84847d09db4f239d0e62db8e0184f26ec26
rebin <rebin.mahmed@gmail.com>
2022-12-21 14:17:48 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,9 @@
 
         <title>Welcome to PHP</title>
 
+
+        <title>Not Welcome</title>
+
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
