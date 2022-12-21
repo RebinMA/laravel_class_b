d8019f48186c1fe10f1e808ad5bff5f7ece1cc22
Rebin <rebin.mahmed@gmail.com>
2022-12-21 14:06:16 +0300

    new files

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBlogRequest;
+use App\Http\Requests\UpdateBlogRequest;
+use App\Models\Blog;
+use Illuminate\Support\Facades\Gate;
+
+
+use Illuminate\Support\Facades\Auth;
+
+class BlogController extends Controller
+{
+
+//    public function __construct()
+//    {
+//        $this->middleware('auth');
+//    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        return view('blank');
+
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBlogRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBlogRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Blog $blog)
+    {
+        if(Gate::allows('edit-posts'))
+            return view('blank');
+        else return ('You dont have permission');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBlogRequest  $request
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBlogRequest $request, Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        //
+    }
+}
---
+++ resources/views/blank.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('title','The new Title')
+
+@section('content')
+    <h1>This is the new content</h1>
+@endsection
